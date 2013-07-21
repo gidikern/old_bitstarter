2b8539401836d91210ca532138f2e4ce76bb60cd
Gidi Kern <gidikern@gmail.com>
2013-07-21 17:58:24 +0300

    Updating  node-js-sample/web.js to call index.html

--- node-js-sample/web.js
+++ node-js-sample/web.js
@@ -1,12 +1,15 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	var buf = fs.readFileSync("index.html");
+    var mystr = buf.toString("utf-8");
+    response.send(mystr);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });

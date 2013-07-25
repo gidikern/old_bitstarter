e482ac2de744f17c434bcbdf4b7a07402eba7968
Gidi Kern <gidikern@gmail.com>
2013-07-25 12:17:22 +0300

    HW4 new index.html

--- web.js
+++ web.js
@@ -1,15 +1,24 @@
 var fs = require('fs');
 var express = require('express');
+var path = require('path');
 
 var app = express.createServer(express.logger());
+console.log("__dirname" + __dirname);
 
-app.get('/', function(request, response) {
-	var buf = fs.readFileSync("index.html");
-    var mystr = buf.toString("utf-8");
-    response.send(mystr);
+
+app.configure( function () {
+  app.use('/css', express.static(__dirname + '/css'));
+  app.use('/js', express.static(__dirname + '/js'));
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function (request, response) {
+  var buf = fs.readFileSync("index.html");
+  var mystr = buf.toString("utf-8");
+  response.send(mystr);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+app.listen(port, function () {
+  console.log("Listening on " + port);
 });
